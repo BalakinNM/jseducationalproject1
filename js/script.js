b1e4cbcaad416a7762ae42a37b1b506a6ce59572
Balakin_N_M <Balakinnm@mail.ru>
2022-11-20 14:27:17 +0300

    third practic

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
 // practic 3
 
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
 'use strict';
 
-const numberOfFilms = +prompt(`Сколько фильмов вы уже посмотрели?`, ``);
+let numberOfFilms;
+function start() {
+    numberOfFilms = +prompt(`Сколько фильмов вы уже посмотрели?`, ``);
+    while (numberOfFilms == `` || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt(`Сколько фильмов вы уже посмотрели?`, ``);
+    }
+}
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -12,26 +31,63 @@ let personalMovieDB = {
     privat: false,
 };
 
-if (personalMovieDB.count < 10) {
-    alert (`Просмотрено довольно мало фильмов`);
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert ("Вы классический зритель"); 
-} else if (personalMovieDB.count >= 30) {
-    alert ("Вы киноман");
-} else {
-    alert ("Произошла ошибка");
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        alert (`Просмотрено довольно мало фильмов`);
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert ("Вы классический зритель"); 
+    } else if (personalMovieDB.count >= 30) {
+        alert ("Вы киноман");
+    } else {
+        alert ("Произошла ошибка");
+    }
 }
 
-for (let i = 0; i < 2; i++) {
-    let filmName =  prompt (`Один из последних просмотренных фильмов?`, ``);
-    let filmGrade = +prompt (`'На сколько оцените его?`, ``);
-    if (filmName != null && filmGrade != null && filmName != `` && filmGrade != `` && filmName.length < 50) {
-        personalMovieDB.movies[filmName] = filmGrade;
-    } else {
-        i--;
-        console.log(`error`);
-    }   
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        let filmName =  prompt(`Один из последних просмотренных фильмов?`, ``);
+        let filmGrade = +prompt(`На сколько оцените его?`, ``);
+        if (filmName != null && filmGrade != null && filmName != `` && filmGrade != `` && filmName.length < 50) {
+            personalMovieDB.movies[filmName] = filmGrade;
+        } else {
+            i--;
+            console.log(`error`);
+        }   
+    }
 }
+
+/* function showMyDB() {
+    if (personalMovieDB.privat == false) {
+        alert(`У вас нет доступа к данным`);
+    } else {
+        console.log(personalMovieDB);
+    }
+} */
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    } 
+}
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        personalMovieDB.geners[i - 1] = prompt(`Ваш любимый жанр под номером ${i}.`, '');
+    }
+}
+
+
+
+// start();
+// detectPersonalLevel();
+// rememberMyFilms();
+// showMyDB(personalMovieDB.privat);
+writeYourGenres();
+console.log(personalMovieDB);
+
+
+
+
 
 
 // practic 2
